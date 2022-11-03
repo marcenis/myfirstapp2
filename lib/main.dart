59700355971d3f284444f39feda4ac9cf0ed0c6a
MARCELLO NISI <marcenis62@gmail.com>
2022-11-03 23:40:35 +0100

    aggiunte immagini colonna

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -23,15 +24,82 @@ class _MyAppState extends State<MyApp > {
           ),
           backgroundColor: Colors.blueAccent,
         ),
-        body: const SafeArea( child: Center(
-            child: Image(
-              image: AssetImage('images/logo_fantacalcio.png'),
-            ),
-          ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                padding: EdgeInsets.all(0),
+                child: Image(
+                  image: NetworkImage('https://d2lhpso9w1g8dk.cloudfront.net/web/img/socialfg.jpg'),
+                ),
+                color: Colors.white,
+                height: 150,
+                width: 150,
+                  ),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                padding: EdgeInsets.all(0),
+                child: Image(
+                  image: NetworkImage('https://content.fantacalcio.it/web/img/medium/live-9-ec22db23-d579-43d4-82db-359678a27cc5.jpg'),
+                ),
+                color: Colors.white,
+                height: 150,
+                width: 150,
+              ),
+              Container(
+                margin: EdgeInsetsDirectional.fromSTEB(10, 5, 10, 5),
+                padding: EdgeInsets.all(0),
+                child: Image(
+                  image: NetworkImage('https://calciodangolo.com/wp-content/uploads/2022/02/I-migliori-giocatori-della-26a-giornata-di-Fantacalcio.png'),
+                  ),
+                color: Colors.white,
+                height: 150,
+                width: 150,
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                padding: EdgeInsets.all(0),
+                child: Image(
+                  image: NetworkImage('https://content.fantacalcio.it/web/img/app/applicazioni-social.jpg'),
+                  ),
+                color: Colors.white,
+                height: 150,
+                width: 150,
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                padding: EdgeInsets.all(0),
+                child: Image(
+                  image: NetworkImage('https://content.fantacalcio.it/web/img/large/novita-fantacalcio-leghe-18267a67-6c51-4040-b7e0-d264fbc713ac.jpg'),
+                ),
+                color: Colors.white,
+                height: 150,
+                width: 150,
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                padding: EdgeInsets.all(0),
+                child: Image(
+                  image: NetworkImage('https://www.guidaconsumatore.com/wp-content/uploads/2016/02/come_giocare_fantacalcio-600x338.jpg'),
+                ),
+                color: Colors.white,
+                height: 150,
+                width: 150,
+              ),
+                ],
+          // const SafeArea( child: Center(
+              //child: Image(
+                // image: AssetImage('images/logo_fantacalcio.png'),
+              ),
         ),
-        backgroundColor: Colors.white30,
-      ),
-    );
+          ),
+        );
+        //backgroundColor: Colors.white30,
+     // ),
+    //);
 
   }
 }
